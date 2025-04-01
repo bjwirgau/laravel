cb8b885dda4f2a9047d7681add864051f90ec68f
Brandon Wirgau <brandonjoelwirgau@gmail.com>
2025-04-01 16:42:45 -0600

    Section 3 Lesson 28 - Pagination

--- src/routes/web.php
+++ src/routes/web.php
@@ -23,8 +23,7 @@ Route::get('/', function() {
 
 Route::get('/tasks', function ()  {
     return view('index', [
-        'tasks' => \App\Models\Task::latest()
-            ->get()
+        'tasks' => Task::latest()->paginate(10)
     ]);
 })->name('tasks.index');
 
